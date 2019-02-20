ec3262acf8a64f74624a89d6009a531ab66ad760
Rekha55 <panwarrekha55@gmail.com>
2019-02-20 18:02:27 +0530

    constraint

--- app/src/main/java/com/example/valuationreport/Registration.java
+++ app/src/main/java/com/example/valuationreport/Registration.java
@@ -4,108 +4,225 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
-import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Registration extends AppCompatActivity {
 
-
-    EditText nameEditText , emailEditText , passwordEditText , mobileEditText , addressEditText;
+    private FirebaseFirestore db;
+    TextView showTextView;
+    EditText nameEditText, emailEditText, passwordEditText, mobileEditText, addressEditText;
     EditText companyCodeEditText;
     Spinner dropdownRegistration;
-    String[] items;
-    String name , email, password ,  address;
-    double mobileNo , companycode;
-    public static final String name_key="rekha", email_key="panwarrekha55@gmail.com", password_key ="rekhapanwar55", address_key="aghduhyjsdgybkasjufhgskjf";
-    public static final  double companyCode_key=8;
-    public static final BigInteger mobile_key = new BigInteger("9924474945");
-   // private DocumentReference mdocRef = FirebaseFirestore.getInstance().collection("sampleData").document("DataField/sampleData4/dataafield4/sampledata5/datafield5");
+    String[] items;                          //for spinner items
+    String name, email, password, address, mobileNo, companyCode;               //for getting and setting input from/to the user
+    int dummyvalue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        dropdownRegistration = findViewById(R.id.registrationSpinnerId);
-        items = new String[]{"Client", "Employee"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdownRegistration.setAdapter(adapter);
+        try {
+            FirebaseApp.initializeApp(this);                    //initialize firebase
+            db = FirebaseFirestore.getInstance();  //firestore object
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Probleem in Object--------------->" + e.getMessage(), Toast.LENGTH_LONG).show();
+            Log.d("FIRESTORE", e.getMessage());
+        }
 
         id();  //id's of all the element
 
-        //inputValue();   //get the input from user
+        setSpinner();  //setting the value of the spinner and applying condition for company code for client
 
-
-
-        //SaveDataInFirestore();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("1st------>>>>>>>>>", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("error", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 
-    public void id(){
-        nameEditText = (EditText)findViewById(R.id.registrationNameId);
-        emailEditText = (EditText)findViewById(R.id.registrationEmailId);
-        passwordEditText = (EditText)findViewById(R.id.registrationPasswordId);
-        mobileEditText = (EditText)findViewById(R.id.registrationMobileId);
-        addressEditText = (EditText)findViewById(R.id.registrationAddressId);
-        companyCodeEditText = (EditText)findViewById(R.id.registrationCompanyCodeId);
+    public void setSpinner() {
+
+        dropdownRegistration = findViewById(R.id.registrationSpinnerId);
+        items = new String[]{"Employee", "Client"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdownRegistration.setAdapter(adapter);
+        dropdownRegistration.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // Notify the selected item text
+                companyCodeEditText.setVisibility(View.INVISIBLE);
+                if (position == 1) {
+                    companyCodeEditText.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
-    public void inputValue(){
+    public void id() {
+        showTextView=(TextView)findViewById(R.id.registrationShowId) ;
+        nameEditText = (EditText) findViewById(R.id.registrationNameId);
+        emailEditText = (EditText) findViewById(R.id.registrationEmailId);
+        passwordEditText = (EditText) findViewById(R.id.registrationPasswordId);
+        mobileEditText = (EditText) findViewById(R.id.registrationMobileId);
+        addressEditText = (EditText) findViewById(R.id.registrationAddressId);
+        companyCodeEditText = (EditText) findViewById(R.id.registrationCompanyCodeId);
+    }
+
+
+    public void OnRegisterButtonClick(View view) {
+
         name = nameEditText.getText().toString();
-        email=nameEditText.getText().toString();
-        password= passwordEditText.getText().toString();
-        mobileNo=Integer.parseInt(mobileEditText.getText().toString());
-        address=addressEditText.getText().toString();
-        String text = dropdownRegistration.getSelectedItem().toString();
-        if (text == "Client") {
-            companyCodeEditText.setVisibility(View.VISIBLE);
-            companycode=Integer.parseInt(companyCodeEditText.getText().toString());
-        }
-    }
+        email = nameEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+        address = addressEditText.getText().toString();
+        mobileNo = mobileEditText.getText().toString();
 
-    public void OnRegisterButtonClick(View view){
-      /*  if(name.isEmpty() & email.isEmpty() & password.isEmpty() & mobileEditText.getText().toString().trim().isEmpty()  & address.isEmpty()){
-            return;
-        }else {
-            startActivity(new Intent(Registration.this, LoginActivity.class));
+        if (dropdownRegistration.getSelectedItem() == "Client") {
+            companyCode = companyCodeEditText.getText().toString();
+        } else if(dropdownRegistration.getSelectedItem() == "Employee"){
+            companyCode=setCompanyCode();
         }
-*/
-        startActivity(new Intent(Registration.this, LoginActivity.class));
-    }
-    public void SaveDataInFirestore(){
-        Map<String,Object> dataToSave = new HashMap<String,Object>();
-        dataToSave.put(name_key,name);
-        dataToSave.put(email_key,email);
-        dataToSave.put(password_key,password);
-       /* dataToSave.put(mobile_key,mobileNo);
-        dataToSave.put(address);
-        if(!companyCodeEditText.getText().toString().trim().isEmpty()){
-            dataToSave.put(companyCode_key,companycode);
+        Toast.makeText(getApplicationContext(), companyCodeEditText.getText().toString() + "", Toast.LENGTH_SHORT).show();   //for checking company code input
+
+
+        if (!hasValidationErrors(name, email, password, mobileNo, address)) {
+
+            Map<String, Object> user = new HashMap<>();
+            user.put("name", name);
+            user.put("email", email);
+            user.put("password", password);
+            user.put("mobileNo", mobileNo);
+            user.put("address", address);
+            user.put("companyCode", companyCode);
+            // Add a new document with a generated ID
+
+
         }
 
-      mdocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
-           @Override
-           public void onSuccess(Void aVoid) {
-               Toast.makeText(Registration.this,"successfully saved data",Toast.LENGTH_SHORT).show();
-           }
-       }).addOnFailureListener(new OnFailureListener() {
-           @Override
-           public void onFailure(@NonNull Exception e) {
-               Toast.makeText(Registration.this,"data not saved",Toast.LENGTH_SHORT).show();
-           }
-       });
-       */
     }
+    public class User {
+        private String username;
+        private HashMap<String, Boolean> privacy;
+
+        public User() {}
+
+        public User(String username, HashMap<String, Boolean> privacy) {
+            this.username = username;
+            this.privacy = privacy;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+
+
+        public void setPrivacy(HashMap<String, Boolean> privacy) {
+            this.privacy = privacy;
+        }
+
+    }
+
+    public String setCompanyCode(){
+        companyCodeEditText.addTextChangedListener(new TextWatcher() {  //necessary to implement all the methoda of TextWatcher
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                companyCodeEditText.setText(String.valueOf(0));
+            }
+        });
+        return companyCode;
+    }
+
+    private boolean hasValidationErrors(String name, String email, String password, String mobile, String address) {
+        if (name.isEmpty()) {
+            nameEditText.setError("Name required");
+            nameEditText.requestFocus();
+            return true;
+        }
+
+        if (email.isEmpty()) {
+            emailEditText.setError("Email required");
+            emailEditText.requestFocus();
+            return true;
+        }
+
+        if (password.isEmpty()) {
+            passwordEditText.setError("Password required");
+            passwordEditText.requestFocus();
+            return true;
+        }
+
+        if (mobile.isEmpty()) {
+            mobileEditText.setError("Mobile No. required");
+            mobileEditText.requestFocus();
+            return true;
+        }
+
+        if (address.isEmpty()) {
+            addressEditText.setError("Address required");
+            addressEditText.requestFocus();
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
 }
